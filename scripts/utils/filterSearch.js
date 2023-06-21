d99e5e6b3b0e8760422ca9d55a821d56eacb01b4
Halima-DERRAHI <123747796+Halima-DERRAHI@users.noreply.github.com>
2023-06-21 11:47:36 +0200

    update

--- scripts/utils/filterSearch.js
+++ scripts/utils/filterSearch.js
@@ -3,19 +3,38 @@ function searchRecipe(recipes) {
 	const filterRecipes = [];
 	const inputValue = mainSearch.value.toLowerCase();
 	
-	recipes.forEach(recipe => {
-		const titleExists = recipe.name.toLowerCase().includes(inputValue);
+	for (let i = 0; i < recipes.length; i++ ) {
+		const recipe = recipes[i];
 
-		const recipeIngredientExists = recipe.ingredients.filter(ingredient =>
-			ingredient.ingredient.toLowerCase().includes(inputValue)
-		).length > 0;
-
-		const recipeDescriptionExists = recipe.description.toLowerCase().includes(inputValue);
-
-		if (titleExists || recipeIngredientExists || recipeDescriptionExists) {
-			filterRecipes.push(recipe);
+		// Recherche dans le titre
+		let titleExists = false;
+		if ( recipe.name.toLowerCase().indexOf(inputValue) !== -1 ) {
+			titleExists = true;
 		}
-	});
+
+		// Recherche dans les ingredients
+		let recipeIngredientExists = false;
+		for ( let j = 0; j < recipe.ingredients.length ; j++ ) {
+
+			if ( recipe.ingredients[j].ingredient.toLowerCase().indexOf(inputValue) !== -1 ) {
+				recipeIngredientExists = true;
+			}
+		}
+
+		// Rercherche dans la description
+		let recipeDescriptionExists = false;
+		for ( let k = 0; k < recipe.description.length ; k++ ) {
+
+			if ( recipe.description[k].toLowerCase().indexOf(inputValue) !== -1 ) {
+				recipeDescriptionExists = true;
+			}
+		}
+
+		if ( titleExists || recipeIngredientExists || recipeDescriptionExists ) {
+			filterRecipes.push(recipe);
+			break;
+		}
+	}
 
 	return filterRecipes;
 }
@@ -23,35 +42,53 @@ function searchRecipe(recipes) {
 // Recherche selon les ingrédients
 
 function filterIngredients(recipes, ingredients) {
-
 	const filteredRecipes = [];
-
-	recipes.forEach(recipe => {
+	
+	for (let i = 0; i < recipes.length; i++) {
+		const recipe = recipes[i];
 		let ingredientMatch = true;
 
-		ingredients.forEach(ingredient => {
-			const ingredientFound = recipe.ingredients.map(recipeIngredient => recipeIngredient.ingredient).includes(ingredient);
+		for (let j = 0; j < ingredients.length; j++) {
+			const ingredient = ingredients[j];
+			let ingredientFound = false;
+
+			for (let k = 0; k < recipe.ingredients.length; k++) {
+				const recipeIngredient = recipe.ingredients[k].ingredient;
+
+				if (recipeIngredient === ingredient) {
+					ingredientFound = true;
+					break;
+				}
+			}
 
 			if (!ingredientFound) {
 				ingredientMatch = false;
+				break;
 			}
-		});
+		}
 
 		if (ingredientMatch) {
 			filteredRecipes.push(recipe);
 		}
-	});
+	}
 
 	return filteredRecipes;
-
 }
 
 function searchIngredientItem(ingredientsArray, ingredientInput) {
 
 	const inputValue = ingredientInput.value.toLowerCase();
-	ingredientsArray = ingredientsArray.filter(item => item.toLowerCase().includes(inputValue));
+	const filteredIngredients = [];
+  
+	for (let i = 0; i < ingredientsArray.length; i++) {
+		const item = ingredientsArray[i];
 	
-	return ingredientsArray;
+		if (item.toLowerCase().indexOf(inputValue) !== -1) {
+			filteredIngredients.push(item);
+		}
+	}
+  
+	return filteredIngredients;
 }
 
 // Recherche selon les appareils
@@ -59,56 +96,98 @@ function searchIngredientItem(ingredientsArray, ingredientInput) {
 function filterAppliances(recipes, appliance) {
 
 	const filteredRecipes = [];
-
-	recipes.forEach(recipe => {
-		if (appliance.length === 0 || appliance.includes(recipe.appliance)) {
+		
+	for (let i = 0; i < recipes.length; i++) {
+		const recipe = recipes[i];
+		let appareilMatch = false;
+		
+		if (appliance.length === 0) {
+			appareilMatch = true;
+		} else {
+			for (let j = 0; j < appliance.length; j++) {
+				if (appliance[j] === recipe.appliance) {
+					appareilMatch = true;
+					break;
+				}
+			}
+		}
+		
+		if (appareilMatch) {
 			filteredRecipes.push(recipe);
 		}
-	});
-
+	}
+		
 	return filteredRecipes;
-
 }
 
 function searchApplianceItem(applianceArray, applianceInput) {
 
 	const inputValue = applianceInput.value.toLowerCase();
-	applianceArray = applianceArray.filter(item => item.toLowerCase().includes(inputValue));
+	const filteredAppliance = [];
+  
+	for (let i = 0; i < applianceArray.length; i++) {
+		const item = applianceArray[i];
 	
-	return applianceArray;
+		if (item.toLowerCase().indexOf(inputValue) !== -1) {
+			filteredAppliance.push(item);
+		}
+	}
+  
+	return filteredAppliance;
 }
 
 // Recherche selon les ustensiles
 
 function filterUstensils(recipes, ustensils) {
-	
 	const filteredRecipes = [];
   
-	recipes.forEach(recipe => {
-		let ustensilMatch = true;
+	for (let i = 0; i < recipes.length; i++) {
+		const recipe = recipes[i];
+		let ustensilMatch = false;
 	
-		ustensils.forEach(ustensil => {
-			const ustensilFound = recipe.ustensils.includes(ustensil);
-	
-			if (!ustensilFound) {
-				ustensilMatch = false;
+		if (ustensils.length === 0) {
+			ustensilMatch = true;
+		} else {
+			const recipeUstensils = recipe.ustensils;
+			const numUstensils = recipeUstensils.length;
+  
+			for (let j = 0; j < numUstensils; j++) {
+				const ustensil = recipeUstensils[j];
+				let ustensilFound = false;
+  
+				for (let k = 0; k < ustensils.length; k++) {
+					if (ustensils[k] === ustensil) {
+						ustensilFound = true;
+						break;
+					}
+				}
+  
+				if (ustensilFound) {
+					ustensilMatch = true;
+					break;
+				}
 			}
-		});
-	
+		}
+  
 		if (ustensilMatch) {
 			filteredRecipes.push(recipe);
 		}
-	});
+	}
   
 	return filteredRecipes;
 }
-  
 
 function searchUstensilItem(ustensilsArray, ustensilsInput) {
 
 	const inputValue = ustensilsInput.value.toLowerCase();
-	ustensilsArray = ustensilsArray.filter(item => item.toLowerCase().includes(inputValue));
-	
-	return ustensilsArray;
-}
+	const filteredUstensil = [];
   
+	for (let i = 0; i < ustensilsArray.length; i++) {
+		const item = ustensilsArray[i];
+	
+		if (item.toLowerCase().indexOf(inputValue) !== -1) {
+			filteredUstensil.push(item);
+		}
+	}
+	return filteredUstensil;
+}
